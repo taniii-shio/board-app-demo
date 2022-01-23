1b4bd59522466444f17f867e8b327c1ed27be581
Naoto Shioya <shioya.naoto17@gmail.com>
2022-01-23 11:32:03 +0900

    create post feature

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,9 +1,14 @@
 import Reacr from "react";
+
 import { auth } from "../firebase";
+import TweetInput from "./TweetInput";
+import styles from "./Feed.module.css";
 
 const Feed = () => {
   return (
-    <div>
+    <div className={styles.feed}>
+      Feed
+      <TweetInput />
       <button onClick={() => auth.signOut()}>Logout</button>
     </div>
   );
